b27e95665df117d96d73992f718e38d44e85da86
Your Name <nguyenducanhtuan0602@gmail.com>
2025-03-24 15:36:16 +0700

    oke bro

--- src/main/java/com/example/UniLabPass/controller/FileUploadController.java
+++ src/main/java/com/example/UniLabPass/controller/FileUploadController.java
@@ -1,5 +1,8 @@
 package com.example.UniLabPass.controller;
 import com.example.UniLabPass.service.CloudinaryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,13 +19,15 @@ public class FileUploadController {
         this.cloudinaryService = cloudinaryService;
     }
 
-    @PostMapping("/upload/image")
-    public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file,
+    @PostMapping(value = "/upload/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "", security = {@SecurityRequirement(name = "BearerAuthentication")})
+    public ResponseEntity<?> uploadImage(@RequestPart("file") MultipartFile file,
                                          @RequestParam("folder") String folderName) throws IOException {
         return ResponseEntity.ok(cloudinaryService.uploadFile(file, folderName));
     }
 
-    @PostMapping("/upload/video")
+    @PostMapping(value = "/upload/video", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "", security = {@SecurityRequirement(name = "BearerAuthentication")})
     public ResponseEntity<?> uploadVideo(@RequestParam("file") MultipartFile file,
                                          @RequestParam("folder") String folderName) throws IOException {
         return ResponseEntity.ok(cloudinaryService.uploadVideo(file, folderName));
