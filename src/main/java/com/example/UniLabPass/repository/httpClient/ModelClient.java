9190d688cf6693c680c0d5d30fc0b9e51a74ce3a
Your Name <nguyenducanhtuan0602@gmail.com>
2025-05-02 20:06:40 +0700

    back old src of model

--- src/main/java/com/example/UniLabPass/repository/httpClient/ModelClient.java
+++ src/main/java/com/example/UniLabPass/repository/httpClient/ModelClient.java
@@ -10,10 +10,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 @FeignClient(name = "model-service", url = "${app.services.model}")
 public interface ModelClient {
-    @PostMapping(value = "/model/verify", consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+    @PostMapping(value = "/verify", consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<String> verify(@RequestPart("image1") MultipartFile image1,
                                   @RequestPart("image2") MultipartFile image2);
-    @GetMapping(value = "/model/healthcheck", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/healthcheck", produces = MediaType.APPLICATION_JSON_VALUE)
     String healthcheck();
 }
